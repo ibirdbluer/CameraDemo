5c00bbcc4e07f5c2f14a5ae7466f9ba23f4aa6e0
Gregory Qian <gregoryqian@GregorydeMacBook-Pro.local>
2017-09-02 18:11:53 +0800

    1.fix crash when swipe slider if filter not selected 2. add front camera 3. add torch

--- CameraDemo_OC/CameraViewController.m
+++ CameraDemo_OC/CameraViewController.m
@@ -12,7 +12,9 @@
 
 @interface CameraViewController () <UICollectionViewDelegate, UICollectionViewDataSource, EditImageVCDelegate>{
     GPUImageStillCamera *imageCamera;
-    GPUImageOutput<GPUImageInput> *filter;
+    GPUImageOutput<GPUImageInput> *tmpFilter;
+    GPUImageFilterGroup *filterGroup;
+    
     GPUImageView *iv;
     
     EditImageViewController *imageVC;
@@ -23,6 +25,7 @@
 }
 @property (weak, nonatomic) IBOutlet UISlider *slider;
 @property (weak, nonatomic) IBOutlet UICollectionView *actionCollectionView;
+@property (weak, nonatomic) IBOutlet UIButton *torchButton;
 
 @end
 
@@ -52,20 +55,25 @@
     self.actionCollectionView.collectionViewLayout = layout;
 
     
-    imageCamera=[[GPUImageStillCamera alloc] initWithSessionPreset:AVCaptureSessionPreset1920x1080 cameraPosition:AVCaptureDevicePositionBack];
+    
+    imageCamera=[[GPUImageStillCamera alloc] initWithSessionPreset:AVCaptureSessionPresetPhoto cameraPosition:AVCaptureDevicePositionBack];
     //AVCaptureDevicePositionBack为后摄像头 front为前置摄像头
     //AVCaptureSessionPreset1920x1080为分辨率 另外还支持多种分辨率
-    //AVCaptureSessionPreset1280x720 等等等等
     // AVCaptureSessionPresetPhoto
     
     imageCamera.outputImageOrientation=UIInterfaceOrientationPortrait;
+    imageCamera.horizontallyMirrorFrontFacingCamera = YES;
+    imageCamera.horizontallyMirrorRearFacingCamera = NO;
     
-    filter=[[GPUImageFilter alloc] init]; //默认滤镜.
+    tmpFilter = [[GPUImageFilter alloc] init];
+    filterGroup = [[GPUImageFilterGroup alloc] init];
+    GPUImageFilter *filter = [[GPUImageFilter alloc] init]; //默认滤镜.
+    [self addGPUImageFilter:filter];
     /*官方版本如下:
      GPUImageFilter *customFilter = [[GPUImageFilter alloc] initWithFragmentShaderFromFile:@"CustomShader"];
      这个CustomShader是自定义的滤镜文件.如果没有,那就使用默认滤镜
      */
-    [imageCamera addTarget:filter];
+    [imageCamera addTarget:filterGroup];
     
     iv=[[GPUImageView alloc] initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, self.view.bounds.size.height-128)];
     iv.fillMode=kGPUImageFillModePreserveAspectRatioAndFill;
@@ -76,11 +84,37 @@
      kGPUImageFillModePreserveAspectRatioAndFill     // Maintains the aspect ratio of the source image, zooming in on its center to fill the view
      };
      */
-    [filter addTarget:iv];
+    [filterGroup addTarget:iv];
     [self.view insertSubview:iv atIndex:0];
-//    [self.view addSubview:iv];//将显示view加入父类
     [imageCamera startCameraCapture];//开启摄像头
 }
+
+//添加滤镜
+- (void)addGPUImageFilter:(GPUImageOutput<GPUImageInput> *)filter
+{
+    [filterGroup addFilter:filter];
+    
+    GPUImageOutput<GPUImageInput> *newTerminalFilter = filter;
+    
+    NSInteger count = filterGroup.filterCount;
+    
+    if (count == 1)
+    {
+        filterGroup.initialFilters = @[newTerminalFilter];
+        filterGroup.terminalFilter = newTerminalFilter;
+        
+    } else
+    {
+        GPUImageOutput<GPUImageInput> *terminalFilter    = filterGroup.terminalFilter;
+        NSArray *initialFilters                          = filterGroup.initialFilters;
+        
+        [terminalFilter addTarget:newTerminalFilter];
+        
+        filterGroup.initialFilters = @[initialFilters[0]];
+        filterGroup.terminalFilter = newTerminalFilter;
+    }
+}
+
 
 
 - (void)didReceiveMemoryWarning {
@@ -90,14 +124,47 @@
 
 
 #pragma mark - Button Action
+// 闪光灯
+- (IBAction)torchSwitch:(UIButton *)sender {
+    if (imageCamera.inputCamera.position == AVCaptureDevicePositionBack) {
+        
+        if (sender.selected) {
+            
+            [imageCamera.inputCamera lockForConfiguration:nil];
+            [imageCamera.inputCamera setTorchMode:AVCaptureTorchModeOff];
+            [imageCamera.inputCamera unlockForConfiguration];
+            
+        }else{
+            
+            [imageCamera.inputCamera lockForConfiguration:nil];
+            [imageCamera.inputCamera setTorchMode:AVCaptureTorchModeOn];
+            [imageCamera.inputCamera unlockForConfiguration];
+        }
+        
+        sender.selected = !sender.selected;
+        
+    }
+}
+
 
 // 翻转摄像头
 - (IBAction)switchCamera:(UIButton *)sender {
+    [imageCamera rotateCamera];
+    
+    if (!sender.selected) {
+        // 当前使用前置摄像头
+        self.torchButton.hidden = YES;
+    }else {
+        // 当前使用后置摄像头
+        self.torchButton.hidden = NO;
+    }
+    sender.selected = !sender.selected;
+
 }
 
 // 获取图片
 - (IBAction)takePhoto:(UIButton *)sender {
-    [imageCamera capturePhotoAsImageProcessedUpToFilter:filter withCompletionHandler:^(UIImage *processedImage, NSError *error) {
+    [imageCamera capturePhotoAsImageProcessedUpToFilter:filterGroup withCompletionHandler:^(UIImage *processedImage, NSError *error) {
         if(error){
             return;
         }
@@ -121,6 +188,11 @@
 - (__kindof UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
     CollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"Cell" forIndexPath:indexPath];
     NSString *title;
+    if (indexPath.row == selectedIndex) {
+        cell.titleLabel.textColor = [UIColor redColor];
+    }else {
+        cell.titleLabel.textColor = [UIColor blackColor];
+    }
     switch (indexPath.row) {
         case 0:
             title = @"照片";
@@ -141,15 +213,25 @@
 }
 
 - (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+    
+    self.slider.hidden = YES;
     selectedIndex = indexPath.row;
+    [collectionView reloadData];
+    
+    GPUImageCropFilter *cropFilter = [[GPUImageCropFilter alloc] initWithCropRegion:CGRectMake(0.0, 0.0, 1.0, 1.0)];
+    
     switch (indexPath.row) {
+
             // 长方形照片
         case 0:
             iv.frame = CGRectMake(0, 0, self.view.bounds.size.width, self.view.bounds.size.height-128);
             break;
         case 1:
             // 正方形照片
+
             iv.frame = CGRectMake(0, 0, self.view.bounds.size.width, self.view.bounds.size.width);
+            tmpFilter = cropFilter;
+
             break;
         case 2:
             // “ 滤镜 ”
@@ -158,8 +240,11 @@
             break;
         default:
             // 各种滤镜
+
+            
+            
             [imageCamera removeAllTargets];
-            [filter removeAllTargets];
+            [filterGroup removeAllTargets];
             self.slider.hidden = NO;
 
             for (int i = 3; i < filterArray.count + 3; i++) {
@@ -169,7 +254,7 @@
                     [self.slider setMaximumValue:4.0];
                     [self.slider setValue:1.0];
                     
-                    filter = [[GPUImageContrastFilter alloc] init];
+                    tmpFilter = [[GPUImageContrastFilter alloc] init];
                     break;
   
                 }else if ([filterName isEqualToString:@"Brightness"]) {
@@ -177,45 +262,47 @@
                     [self.slider setMaximumValue:1.0];
                     [self.slider setValue:0.0];
                     
-                    filter = [[GPUImageBrightnessFilter alloc] init];
+                    tmpFilter = [[GPUImageBrightnessFilter alloc] init];
                     break;
                 }else if ([filterName isEqualToString:@"RGB"]) {
                     [self.slider setMinimumValue:0.0];
                     [self.slider setMaximumValue:2.0];
                     [self.slider setValue:1.0];
                     
-                    filter = [[GPUImageRGBFilter alloc] init];
+                    tmpFilter = [[GPUImageRGBFilter alloc] init];
                     break;
                 }else if ([filterName isEqualToString:@"Hue"]) {
                     [self.slider setMinimumValue:0.0];
                     [self.slider setMaximumValue:360.0];
                     [self.slider setValue:90.0];
                     
-                    filter = [[GPUImageHueFilter alloc] init];
+                    tmpFilter = [[GPUImageHueFilter alloc] init];
                     break;
                 }else if ([filterName isEqualToString:@"Exposure"]) {
                     [self.slider setMinimumValue:-4.0];
                     [self.slider setMaximumValue:4.0];
                     [self.slider setValue:0.0];
                     
-                    filter = [[GPUImageExposureFilter alloc] init];
+                    tmpFilter = [[GPUImageExposureFilter alloc] init];
                     break;
                 }else if ([filterName isEqualToString:@"Gamma"]) {
                     [self.slider setMinimumValue:0.0];
                     [self.slider setMaximumValue:3.0];
                     [self.slider setValue:1.0];
                     
-                    filter = [[GPUImageGammaFilter alloc] init];
+                    tmpFilter = [[GPUImageGammaFilter alloc] init];
                     break;
                 }
 
             }
+            filterGroup = [[GPUImageFilterGroup alloc] init];
+
+            [self addGPUImageFilter:tmpFilter];
             
-            [imageCamera addTarget:filter];
+            [imageCamera addTarget:filterGroup];
             
             imageCamera.runBenchmark = YES;
-            
-            [filter addTarget:iv];
+            [filterGroup addTarget:iv];
             [imageCamera startCameraCapture];
             
             break;
@@ -227,27 +314,27 @@
         [imageCamera resetBenchmarkAverage];
         NSString *filterName = filterArray[selectedIndex - i];
         if ([filterName isEqualToString:@"Contrast"]) {
-            [(GPUImageContrastFilter *)filter setContrast:[sender value]];
+            [(GPUImageContrastFilter *)tmpFilter setContrast:[sender value]];
             break;
 
         }else if ([filterName isEqualToString:@"Brightness"]) {
-            [(GPUImageBrightnessFilter *)filter setBrightness:[sender value]];
+            [(GPUImageBrightnessFilter *)tmpFilter setBrightness:[sender value]];
             break;
 
         }else if ([filterName isEqualToString:@"RGB"]) {
-             [(GPUImageRGBFilter *)filter setGreen:[sender value]];
+             [(GPUImageRGBFilter *)tmpFilter setGreen:[sender value]];
             break;
 
         }else if ([filterName isEqualToString:@"Hue"]) {
-            [(GPUImageHueFilter *)filter setHue:[sender value]];
+            [(GPUImageHueFilter *)tmpFilter setHue:[sender value]];
             break;
 
         }else if ([filterName isEqualToString:@"Exposure"]) {
-             [(GPUImageExposureFilter *)filter setExposure:[sender value]];
+             [(GPUImageExposureFilter *)tmpFilter setExposure:[sender value]];
             break;
 
         }else if ([filterName isEqualToString:@"Gamma"]) {
-            [(GPUImageGammaFilter *)filter setGamma:[sender value]];
+            [(GPUImageGammaFilter *)tmpFilter setGamma:[sender value]];
             break;
 
         }
